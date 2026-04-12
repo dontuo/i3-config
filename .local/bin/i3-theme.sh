31f173d5c05ed9eba33bdcf09463b3e9be61932e
dontuo <bench.oleg@gmail.com>
2026-04-13 01:29:00 +0300

    fix some bugs

--- .local/bin/i3-theme.sh
+++ .local/bin/i3-theme.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
-# Extract colors from Xresources and write them to a temp file i3 can include
-xrdb -query | grep "*color" | sed 's/*//' > /tmp/i3-colors
+#!/bin/sh
+
+# 1. Extract colors from Xresources and format them for i3
+# This converts: *color0: #123456  --> set $color0 #123456
+xrdb -query | grep -E "^\*color[0-9]" | sed 's/*//; s/:/ /; s/^/set $/' > /tmp/i3-colors
+
+# 2. Add some custom aliases if you want
+# This way you can use $bg in your config instead of $color0
+echo "set \$bg \$color0" >> /tmp/i3-colors
+echo "set \$fg \$color7" >> /tmp/i3-colors
+
+# 3. Reload i3 to apply the new file
+i3-msg reload
