caac6de3c35c930da1e14ae2f4e5d235703889bd
dontuo <bench.oleg@gmail.com>
2026-04-13 20:21:04 +0300

    update dmenu. Now it uses colors from xresources

--- dmenu/config.h
+++ dmenu/config.h
@@ -7,6 +7,13 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* In config.h */
+static const char *xres[] = {
+    "dmenu.background",
+    "dmenu.foreground",
+    "dmenu.selbackground",
+    "dmenu.selforeground",
+};
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
